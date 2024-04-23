4d864220d0123c7de7a0fb9af1ca62edef12831c
Vollachia <ayhammuati@gmail.com>
2024-04-23 20:48:36 +0300

    Added First and Last name and Date of birth to application user haven't done anything proper to implement them

--- BagOfManyThings/BagOfManyThings/Data/ApplicationUser.cs
+++ BagOfManyThings/BagOfManyThings/Data/ApplicationUser.cs
@@ -5,6 +5,12 @@ namespace BagOfManyThings.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [PersonalData]
+        public string? FirstName { get; set; }
+        [PersonalData]
+        public string? LastName { get; set; }
+        [PersonalData]
+        public DateOnly? DateOfBirth { get; set; }
     }
 
 }
